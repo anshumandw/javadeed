026c2445d995028bef410d75c8c3e79a8d92ec53
anshumandw <102248548+anshumandw@users.noreply.github.com>
2023-07-25 22:59:37 +0530

    New XMeme Project

---
+++ MemeProject/MemeProject/src/main/java/com/xmeme/MemeProject/MemeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.xmeme.MemeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeProjectApplication.class, args);
+	}
+
+}
---
+++ MemeProject/MemeProject/src/test/java/com/xmeme/MemeProject/MemeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmeme.MemeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
